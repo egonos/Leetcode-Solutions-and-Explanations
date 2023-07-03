459559c58f90f66297b836c746f72286572df34b
Egemen Dalgıç <egedalgic@gmail.com>
2023-07-03 12:26:38 +0300

    ...

--- Easy Problems/Gas Station(134).py
+++ Easy Problems/Gas Station(134).py
@@ -1,11 +1,4 @@
 """
-There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
-
-You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
-
-Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
-
-
 Hi everyone! For this problem, I needed some time to understand the logic behind the solution so I'll do my best to explain it.
 
 We are trying to find the starting position -if any- which gives us a chance to visit all the other stations. For the placeholder we will use starting_position. It's initial value is 0 meaning we will try each station one by one.
