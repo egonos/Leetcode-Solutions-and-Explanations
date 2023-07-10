3c63909ce2ff92d1d71d71195529f92b0d5d2bc8
Egemen Dalgıç <egedalgic@gmail.com>
2023-07-10 06:56:29 +0300

    ...

--- Easy Problems/Queries Quality and Percentage(1211).sql
+++ Easy Problems/Queries Quality and Percentage(1211).sql
@@ -8,4 +8,4 @@ Sum all the ones and divide it into the number of occurances for each group.
 Final Code*/
 
 select query_name, round(avg(rating/position),2) as quality, round(100*sum(if (rating < 3, 1,0))/count(*),2)  as poor_query_percentage from queries
-group by query_name;Queries Quality and Percentage(1211(
+group by query_name;
