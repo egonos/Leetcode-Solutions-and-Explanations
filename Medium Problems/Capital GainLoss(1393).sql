3f1932990e6277fe696232c79f33ad7b0d1539bc
Egemen Dalgıç <egedalgic@gmail.com>
2023-07-16 11:14:03 +0300

    ...

--- Medium Problems/Capital GainLoss(1393).sql
+++ Medium Problems/Capital GainLoss(1393).sql
@@ -1,9 +1,9 @@
 /*
 Hi everyone. Here is how I've solved this question.
-We have to reassign the price values according to the Buy-Sell conditions.
-For that purpose I've constructed anouther table with the stock_name and the net_price.
+We have to reassign the price values according to the Buy-Sell conditions. (Buying is costly so -price <- price)
+For that purpose I've constructed another table with the `stock_name` and the `net_price`.
 I've used the CASE statement to reassign the price values.
-Then I've grouped the net_price values by stock_name and calculated the sum of the net_price values.
+Then I've grouped the net_price values by `stock_name` and calculated the sum of the `net_price` values.
 */
 
 
