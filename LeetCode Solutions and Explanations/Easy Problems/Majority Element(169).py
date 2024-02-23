25425a71d8adbc4632c507121f144394ed323b6d
Egemen Dalgıç <egedalgic@gmail.com>
2024-02-23 16:22:45 +0300

    ...

--- LeetCode Solutions and Explanations/Easy Problems/Majority Element(169).py
+++ LeetCode Solutions and Explanations/Easy Problems/Majority Element(169).py
@@ -1,27 +1,13 @@
-"""
-Intuition
-For this problem, in my first attempt, I've utilized Counter() object for find the majority element:
-"""
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        
-        counter = collections.Counter(nums)
-        limit = len(nums)/2
-        
-        for i,j in counter.items(): 
-            if j>limit: return i
-
-"""
-Then to improve the efficiency I constructed the count dictionary from scratch. The mian difference between the two is second one immediately stops when it finds the mojority element whereas the first one completes its job then looks up the majority element. The time complexity is O(n) and the space complexity is also O(n).
-"""
-
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        
-        count_dict = {}
-        limit = len(nums)/2
+        n = len(nums) / 2
+        record_dict = {}
         for num in nums:
-            if num not in count_dict: count_dict[num] = 1
-            else: 
-                count_dict[num]+=1
-            if count_dict[num] > limit: return num
+            if num not in record_dict:
+                record_dict[num] = 1
+
+            else:
+                record_dict[num] += 1
+
+            if record_dict[num] > n:
+                return num
--- LeetCode Solutions and Explanations/Medium Problems/Rotate Array(189).py
+++ LeetCode Solutions and Explanations/Medium Problems/Rotate Array(189).py
@@ -1,15 +1,10 @@
-"""
-Intuition
-My code is not the most efficient yet it is really easy to understand. Basically for k times it pops the last index and inserts at the beginning of the array.
-Time complexity: O(n*k)
-Space complexity: O(1)
-
-"""
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        while k:
-            nums.insert(0,nums.pop())
-            k-=1
+        rotate = k % len(nums)
+        toLeft = nums[-rotate:]
+        toRight = nums[:len(nums)-rotate]
+        
+        nums[:rotate],nums[rotate:] = toLeft,toRight
