231b335c42a3184f5a2477b7a3ec4b4d0efef607
Egemen Dalgıç <egedalgic@gmail.com>
2024-02-23 13:04:46 +0300

    ...

--- LeetCode Solutions and Explanations/Medium Problems/Remove Duplicates from Sorted Array II(80).py
+++ LeetCode Solutions and Explanations/Medium Problems/Remove Duplicates from Sorted Array II(80).py
@@ -1,22 +1,15 @@
-"""
-Intuition
-Hi everyone! For this question, I followed a similar strategy with the easier one. The only difference is that I employed a boolean variable to check duplicate values.
-"""
-
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        pointer = 1
-        duplicate = False
-        while pointer<len(nums):
-            if not duplicate and nums[pointer] == nums[pointer-1]:
-                duplicate = True
-                pointer+=1
+        pointer = 0
+        memory = set()
+        while pointer < len(nums):
+            if pointer < len(nums) - 1 and nums[pointer] == nums[pointer + 1] and nums[pointer] not in memory:
+                memory.add(nums[pointer])
+            
+            elif pointer < len(nums) - 1 and nums[pointer] == nums[pointer + 1] and nums[pointer] in memory:
+                while pointer < len(nums) - 1 and nums[pointer] == nums[pointer+1]:
+                    nums.pop(pointer + 1)
 
-            elif duplicate and nums[pointer] == nums[pointer-1]:
-                nums.remove(nums[pointer])
-
-            else:
-                duplicate = False
-                pointer+=1
+            pointer += 1
 
         return len(nums)
