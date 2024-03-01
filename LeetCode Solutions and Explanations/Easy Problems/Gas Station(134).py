8f2773fb62a7a5a80e9eb9ceebc30a0c9d47077b
Egemen Dalgıç <egedalgic@gmail.com>
2024-03-01 16:39:18 +0300

    ...

--- LeetCode Solutions and Explanations/Easy Problems/Gas Station(134).py
+++ LeetCode Solutions and Explanations/Easy Problems/Gas Station(134).py
@@ -1,33 +1,45 @@
-"""
-Hi everyone! For this problem, I needed some time to understand the logic behind the solution so I'll do my best to explain it.
-
-We are trying to find the starting position -if any- which gives us a chance to visit all the other stations. For the placeholder we will use starting_position. It's initial value is 0 meaning we will try each station one by one.
-
-To see the possibility for traveling through all the stations, we will define a base condition:
-
-if sum(gas) - sum(cost) < 0: return -1.
-Now, after make sure that a solution exists, we will loop through the arrays.temp_gas is the gas balance when we start at station i. When it falls below zero it means we have no gas left so we can't traverse all the stations. Here is the fancy part:
-
-if temp_gas < 0:
-        temp_gas = 0
-        starting_position = i + 1
-This part confused me a lot. The first thing came to my mind is how it is possible to increasing starting index by negative number counts allows us to find the solution? Well, from my understanding, we are already sure that a solution exists and when we start at previous indicies we can not maintain a positive balance. When we find an index which keeps the balance positive till the end should be the solution. I hope this explanation is clear for you...
-
-Time complexity: O(n) {looping}
-Space complexity: O(1) {the placeholder}
-"""
-
+#Brute Force Solution
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
 
+        if sum(gas) < sum(cost):
+            return -1
+        
+        for i in range(len(gas)):
+            current = i
+            tank = 0
+            count = 0
+            while tank >=0:
+                
+                tank += (gas[i] - cost[i])
+
+                if i == len(gas) - 1:
+                    i = 0
+
+                else:
+                    i+=1
+
+                count+=1
+
+                if count == len(gas):
+                    return i
+            
+            i = current
+
+        return -1
+
+# Efficient Solution
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
         if sum(gas) - sum(cost) < 0: return -1
 
-        temp_gas = 0
-        starting_position = 0
+        tank = 0
+        start_idx = 0
         for i in range(len(gas)):
-            temp_gas += gas[i] - cost[i]
-            if temp_gas < 0:
-                temp_gas = 0
-                starting_position = i + 1
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                tank = 0
+                start_idx = i+1
 
-        return starting_position
+        return start_idx
