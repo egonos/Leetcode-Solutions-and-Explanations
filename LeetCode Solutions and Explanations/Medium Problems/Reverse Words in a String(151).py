502e4ea2e809db95fb5c40337b73a62683b5398d
Egemen Dalgıç <egedalgic@gmail.com>
2024-03-04 12:38:14 +0300

    ...

--- LeetCode Solutions and Explanations/Medium Problems/Reverse Words in a String(151).py
+++ LeetCode Solutions and Explanations/Medium Problems/Reverse Words in a String(151).py
@@ -1,3 +1,17 @@
+#1st Solution
 class Solution:
     def reverseWords(self, s: str) -> str:
-        return ' '.join(s.split()[::-1])
+        s_split = s.strip().split()
+        return ' '.join(s_split[::-1])
+
+#2nd Solution
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s_split = s.strip().split()
+        left,right = 0,len(s_split)-1
+        while left < right:
+            s_split[left],s_split[right] = s_split[right], s_split[left]
+            right-=1
+            left+=1
+
+        return ' '.join(s_split)
