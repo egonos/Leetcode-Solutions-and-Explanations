28d8275a917b27c4f64b67489bbeffc7e1ee1187
Egemen Dalgıç <egedalgic@gmail.com>
2024-03-09 06:43:31 +0300

    ...

--- LeetCode Solutions and Explanations/Easy Problems/Longest Common Prefix(14).py
+++ LeetCode Solutions and Explanations/Easy Problems/Longest Common Prefix(14).py
@@ -1,36 +1,23 @@
-"""
-Intuition
-After defining the base conditions we can iteratively increase the length of the prefix and check it's validity by comparing across all the words.
-
-Approach
-``startswith()`-> To check the validity
-
-while -> increase the length of the prefix
-
-Complexity
-Time complexity:
-O(nm) -> m is the average length of the strings
-
-Space complexity:
-O(1)
-"""
-
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 0: return ""
-        if "" in strs: return ""
-        if len(strs) == 1: return strs[0]
+        if len(strs) == 1:
+            return strs[0]
 
-        prefix = ""
-        min_length = min([len(word) for word in strs])
+        pointer = 0
+        common = ""
+        try: 
+            while True:
+                current_common = ""
+                for st in strs:
+                    if not current_common:
+                        current_common += st[pointer]
+                        
+                    elif current_common[-1] != st[pointer]:
+                        return common
 
-        i = 0
-        while i < min_length:
-            candidate = strs[0][0:i+1]
-            if all(word.startswith(candidate) for word in strs[1:]):
-                i += 1
-                prefix = candidate
-            else:
-                return prefix
+                    
+                common += current_common
+                pointer+=1
 
-        return prefix
+        except:
+            return common
