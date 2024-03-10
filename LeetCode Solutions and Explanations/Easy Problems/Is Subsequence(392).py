9ea51ec3f397faeb17be2f2e8ec8444692c4728b
Egemen Dalgıç <egedalgic@gmail.com>
2024-03-10 18:21:29 +0300

    ...

--- LeetCode Solutions and Explanations/Easy Problems/Is Subsequence(392).py
+++ LeetCode Solutions and Explanations/Easy Problems/Is Subsequence(392).py
@@ -1,41 +1,14 @@
-"""
-Intuition
-My intuition for this problem is to store the t-indexes of letters found in s then check whether they are monotonically increasing.
-
-Approach
-To find the indexes, I've used find(). I've stored the relevant indexes in ix.
-
-Complexity
-Time complexity:
--> find(): O(n)
-->while: O(n)
-->for: O(n)
-
-resulting: O(n^2)
-
-Space complexity:
-from index storing: O(n)
-
-"""
-
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        
+        if not s: return True
         pointer = 0
-        ix = []
-       
-        start = 0
-        while pointer<len(s):
-            candidate = s[pointer]
-            idx = t.find(candidate,start)
-            if idx == -1: return False
 
-            else:
-                ix.append(idx)
-                pointer+=1
-                start = idx+1
+        for idx in t:
+            if pointer == len(s) - 1 and s[pointer] == idx:
+                return True
 
-        for i in range(1,len(ix)):
-            if ix[i-1] > ix[i]: return False
-
-
-        return True
+            if s[pointer] == idx:
+                pointer +=1
+            
+        return False
