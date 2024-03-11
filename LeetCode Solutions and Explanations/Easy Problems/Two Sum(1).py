97440d4842a11c68cd1533c09fa2809a453368cd
Egemen Dalgıç <egedalgic@gmail.com>
2024-03-11 14:48:31 +0300

    ...

--- LeetCode Solutions and Explanations/Easy Problems/Two Sum(1).py
+++ LeetCode Solutions and Explanations/Easy Problems/Two Sum(1).py
@@ -1,30 +1,20 @@
-"""
-Intuition
-Instead of trying all the combinations in a nested loop, I calculated the complementary variables and find their index if they are in the array. If it is and the two index are not equal then I returned them
-
-Approach
-The complementary variable doesn't have to be in the array. To handle this, I've used try-except statements. Besides handling the error,except statement allowed me to pass the index comparison part also.
-
-Complexity
-Time complexity:
-O(n^2)
-->from index()
-->from loop
-
-Space complexity:
-O(1)
-"""
-
+# Brute Force Solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for first_idx in range(len(nums)):
+            for second_idx in range(first_idx + 1,len(nums)):
+                if sum([nums[first_idx], nums[second_idx]]) == target:
+                    return [first_idx,second_idx]
 
-        for idx1 in range(len(nums)):
-            complementary = target - nums[idx1]
-            try:
-                idx2 = nums.index(complementary)
+# 2nd Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            complementary = target - nums[i]
 
-            except:
-                continue
+            if complementary in dic:
+                return dic[complementary],i
 
-            if idx1 != idx2:
-                return idx1,idx2
+            else:
+                dic[nums[i]] = i
