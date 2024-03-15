666854ef4e63b76dd6c83f23d21143bc6015945e
Egemen Dalgıç <egedalgic@gmail.com>
2024-03-15 12:53:42 +0300

    ...

--- LeetCode Solutions and Explanations/Easy Problems/Valid Anagram(242).py
+++ LeetCode Solutions and Explanations/Easy Problems/Valid Anagram(242).py
@@ -1,21 +1,9 @@
-"""
-Intuition
-We can create a hash map for each and compare them
-
-Approach
-Using collections.Counter() is the easiest way to create the hashmap we need.
-
-Complexity
-Time complexity:
-O(log(n))
-
-Space complexity:
-O(log(n))
-"""
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict = collections.Counter(s)
-        t_dict = collections.Counter(t)
+        if len(s) != len(t):
+            return False
 
-        return sorted(t_dict.items())==sorted(s_dict.items())
+        sCounts = collections.Counter(s)
+        tCounts = collections.Counter(t)
+        return tCounts == sCounts
+        
