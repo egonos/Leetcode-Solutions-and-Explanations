0d715b04b1e233d22af85bd3a72b3258ed5e681e
Egemen Dalgıç <egedalgic@gmail.com>
2024-03-16 17:04:13 +0300

    ...

--- LeetCode Solutions and Explanations/Medium Problems/House Robber(198).py
+++ LeetCode Solutions and Explanations/Medium Problems/House Robber(198).py
@@ -1,29 +1,8 @@
-"""
-Intuition
-Hi everyone! For this problem, I've utilized dynamic programming approach. The whole code tries maximize the profit based on this condition: We can choose current home and two homes prior or one house before.
-
-dp[1] = max(dp[0],nums[1])
-dp[i] = max(dp[i-1],nums[i]+dp[i-2])
-
-Complexity
-
-Time complexity:
--> Loop: O(n)
-Space complexity:
--> Array: O(n)
-
-"""
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n ==1: return nums[0]
-        elif n == 2: return max(nums)
+        maxMoney = [0] * (len(nums) + 1)
+        maxMoney[1] = nums[0]
+        for i in range(2,len(nums)+1):
+            maxMoney[i] = max(maxMoney[i-1],maxMoney[i-2] + nums[i-1])
 
-        dp = [0]*n
-        dp[0] = nums[0]
-        dp[1] = max(dp[0],nums[1])
-        for i in range(2,n):
-            dp[i] = max(dp[i-1],nums[i]+dp[i-2])
-
-        return dp[-1]
+        return maxMoney[-1]
